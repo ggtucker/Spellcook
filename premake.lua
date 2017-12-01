bf70bc22b24c68a4d1ec00054ca629412b15d0f2
Geoffrey Tucker <gtuck123@yahoo.com>
2017-12-01 01:51:50 -0800

    Added premake configurations for 32 and 64 bit, and added SFML as a dependency. To avoid including any unnecessary local machine data, I also created the local-only file 'premake_config.lua' which contains the user's dependency include and lib paths

--- premake.lua
+++ premake.lua
@@ -1,5 +1,19 @@
+local function prequire(m) 
+    local ok, err = pcall(require, m) 
+    if not ok then return nil, err end
+    return err
+end
+
+local config = prequire "premake_config"
+if not config then
+    print("Error: could not find premake_config.lua. See README.md for setup information.")
+    os.exit()
+end
+print("Loaded premake_config module")
+
 workspace "Spellcook"
     configurations { "Debug", "Release" }
+    platforms { "Win32", "Win64" }
     location "build"
 
 project "Spellcook"
@@ -7,13 +21,26 @@ project "Spellcook"
     language "C++"
     location "build"
     targetdir "build/%{cfg.buildcfg}"
+    includedirs { config.includedirs }
 
-    files { "**.h", "**.cpp" }
+    files { "Source/**.h", "Source/**.cpp" }
 
     filter "configurations:Debug"
-        defines { "DEBUG" }
         symbols "On"
+        defines { "DEBUG", "SFML_STATIC" }
+        links { "sfml-graphics-s-d", "sfml-window-s-d", "sfml-system-s-d", "opengl32", "freetype", "jpeg", "winmm", "gdi32" }
 
     filter "configurations:Release"
-        defines { "NDEBUG" }
         optimize "On"
+        defines { "NDEBUG", "SFML_STATIC" }
+        links { "sfml-graphics-s", "sfml-window-s", "sfml-system-s", "opengl32", "freetype", "jpeg", "winmm", "gdi32" }
+
+    filter { "platforms:Win32" }
+        system "Windows"
+        architecture "x32"
+        syslibdirs { config.syslibdirs32 }
+
+    filter { "platforms:Win64" }
+        system "Windows"
+        architecture "x64"
+        syslibdirs { config.syslibdirs64 }
